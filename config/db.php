c52320635bb36067c6bfaf0799ccf3995ef37f2d
black-bullet <blackbullet@i.ua>
2015-04-15 23:42:27 +0300

    Create project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=projectdb',
+    'username' => 'project',
+    'password' => '12345678',
+    'charset' => 'utf8',
+];
