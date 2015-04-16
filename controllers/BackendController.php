05695bb7501fde2f0b585e1607efe09ae1cb6315
Vitaly Panchuk <vit.panchuk@gmail.com>
2015-04-16 13:12:23 +0400

    comparing algorithm added

--- controllers/BackendController.php
+++ controllers/BackendController.php
@@ -25,7 +25,36 @@ class BackendController extends \yii\web\Controller
     	$request=Yii::$app->request;
     	$post=$request->getBodyParams();
 
-    	$qa =new \app\models\QuestionAnswer();
+    	$qa = new \app\models\QuestionAnswer();
+
+        if ($post['status'] == 1) {
+            $questions = QuestionAnswer::find()
+            ->where(['status' => 2])
+            ->orderBy('count DESC')
+            ->all();
+
+            $comparedQuestions = [];
+            $isComparedQuestions = false;
+
+            foreach ($questions as $qa) {
+                if (levenshtein($qa->question, $post['question']) <= 10) {
+                    $comparedQuestions[] = $qa;
+                    $isComparedQuestions = true;
+                }
+            }
+            if ($isComparedQuestions) {
+                $min = $comparedQuestions[0];
+
+                foreach ($comparedQuestions as $qa) {
+                    if (levenshtein($qa->question, $post['question']) < levenshtein($min->question, $post['question'])) {
+                        $min = $qa;    
+                    }
+                }  
+                return $min;    
+            }
+              
+        }
+        
     	$qa->question=$post['question'];
         if (array_key_exists('answer', $post))
             $qa->answer=$post['answer'];
@@ -36,24 +65,4 @@ class BackendController extends \yii\web\Controller
         
         return $qa;
     }
-
-    public function actionQuery1()
-    {
-        \Yii::$app->response->format='json';
-
-        $request=Yii::$app->request;
-        $post=$request->getBodyParams();
-
-        
-    }
-
-
-
-    // public function getQuery()
-    // {
-    //     $request=Yii::$app->request;
-    //     $post=$request->post();
-    //     $name=$request->post('question');
-    //     return $name;
-    // }
 }
